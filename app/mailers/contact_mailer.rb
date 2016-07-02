2277f1b3dc914588e9c2ee3b0b27d27a0d660545
Anibal Rodriguez <sytek1337@gmail.com>
2016-07-01 19:08:45 -0500

    added mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+
+  default to: 'sytek1337@gmail.com'
+
+  def contact_email(name, email, body)
+      @name = name
+      @email = email
+      @body = body
+
+      mail (from: email, subject: 'Contact message from Coach Match!')
+  end
+end
